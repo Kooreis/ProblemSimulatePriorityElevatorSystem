3f5554d52b9d289b727b588e8fd0c1ba6c761697
Kooreis <kooreis.official@gmail.com>
2024-07-06 09:39:43 -0500

    This is the initial setup of the Elevator class. It includes the SortedSet to hold the floor requests and a variable to keep track of the current floor. The constructor initializes these variables.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,3 @@
-Here is a simple console application in C# that simulates a priority elevator system with multiple floor requests. This application uses a SortedSet to keep track of the floor requests in order of priority.
-
-```C#
 using System;
 using System.Collections.Generic;
 
@@ -14,52 +11,4 @@ public class Elevator
         requests = new SortedSet<int>();
         currentFloor = 0;
     }
-
-    public void RequestFloor(int floor)
-    {
-        requests.Add(floor);
-    }
-
-    public void Operate()
-    {
-        while (requests.Count > 0)
-        {
-            int nextFloor = requests.Min;
-            requests.Remove(nextFloor);
-
-            if (nextFloor > currentFloor)
-            {
-                for (int i = currentFloor; i <= nextFloor; i++)
-                {
-                    Console.WriteLine("Elevator is at floor " + i);
-                }
-            }
-            else
-            {
-                for (int i = currentFloor; i >= nextFloor; i--)
-                {
-                    Console.WriteLine("Elevator is at floor " + i);
-                }
-            }
-
-            currentFloor = nextFloor;
-        }
-    }
 }
-
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        Elevator elevator = new Elevator();
-
-        elevator.RequestFloor(3);
-        elevator.RequestFloor(5);
-        elevator.RequestFloor(1);
-
-        elevator.Operate();
-    }
-}
-```
-
-In this application, the `Elevator` class has a `SortedSet` of floor requests and a `currentFloor` variable. The `RequestFloor` method adds a floor to the set of requests. The `Operate` method processes the requests in order, moving the elevator to each requested floor and removing the request from the set. The `Main` method creates an `Elevator` object, adds some floor requests, and then operates the elevator.
